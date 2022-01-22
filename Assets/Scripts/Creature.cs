116f1e942c13a6d662d80ae2710ca5d1c70f3647
prakhar-s-srivastava <prakhar.s.srivastava@gmail.com>
2022-01-22 17:44:48 +0530

    added scene and scripting test

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature :MonoBehaviour
+{
+    public float Health;
+
+    public void Attack(){}
+    public void Walk(){}
+    public void Idle(){}
+    public void Give(){}
+    public void Recive(){}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl
+{
+   
+
+   public void Move(){}
+   public void Sprint(){}
+   public void Attack(){}
+   public void Jump(){}
+   public void Switch(){}
+}
